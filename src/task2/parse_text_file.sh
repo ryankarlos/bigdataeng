4f9fb8dbf22da38b534ecf06183fce4c47370b71
Ryan Nazareth <ryankarlos@gmail.com>
2022-12-17 23:20:21 +0000

    task2 modify input and commands

--- src/task2/parse_text_file.sh
+++ src/task2/parse_text_file.sh
@@ -8,21 +8,20 @@ function complex()
 {
 for a in `yes | nl | head -50 | cut -f 1`; do \
   head -$(($a*2)) inputfile | tail -1 | \
-  awk 'BEGIN{FS="\t"}{print $2}' | xargs wget -c 2> /dev/null;
+  awk 'BEGIN{FS="\t"}{print $2}' | xargs wget -c ;
 done
 }
 
 
 function simplified()
 {
-  while read -r line ;
+  awk '{print $2}' inputfile | tail -n +2 | while read -r line ;
   do
    if [ ! -z "${line}" ];
    then
       wget -c ${line} 2> /dev/null;
    fi;
-  done < <(cut -f2 inputfile | tail -n +2)
-
+  done
 }
 
 $func
